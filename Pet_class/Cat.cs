2fbf54f0962c544ef57df81692256019e51099c5
Jan-Roger Kviteberg <janrogerk@hotmail.com>
2024-01-28 20:49:18 +0100

    Added loads of comments

--- Pet_class/Cat.cs
+++ Pet_class/Cat.cs
@@ -1,14 +1,16 @@
 ﻿namespace Pet_class;
 
-public class Cat : Pet
+public class Cat : Pet  // Arver fra Pet-klassen.
 {
+    // Konstruktør for Cat som kaller baseklassens konstruktør med 'name' og setter spesifikke egenskaper for en katt.
     public Cat(string name) : base(name)
     {
-        Description = "A white Cat";
-        Trick = "Piss you off";
-        Noise = "Mjau";
+        Description = "A white Cat";  // Settes spesifikk beskrivelse for Cat.
+        Trick = "Piss you off";  // Definerer et triks som er spesifikt for Cat.
+        Noise = "Mjau";  // Angir lyden en katt lager.
     }
-    //Implementing abstract method to Make a Noise
+
+    // Implementerer den abstrakte metoden MakeNoise fra Pet-klassen, spesifikt for Cat.
     public override void MakeNoise()
     {
         Console.WriteLine($"Cat {Name} Make Noise: {Noise}");
